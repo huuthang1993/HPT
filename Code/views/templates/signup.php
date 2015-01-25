736891502690dbf06fcc37e007434c4dd512c7b2
huuthang1993 <huuthang51103305@gmail.com>
2015-01-25 08:45:18 +0700

    update signup.php

--- Code/views/templates/signup.php
+++ Code/views/templates/signup.php
@@ -27,52 +27,52 @@
 							<form role="form">
 								<div class="col-lg-6">
 									<div class="form-group">
-										<label for="InputName">Enter Name</label>
+										<label for="name">Enter Name</label>
 										<div class="input-group">
-											<input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" >
+											<input type="text" class="form-control" name="name" id="name" placeholder="Enter Name" >
 											<span class="input-group-addon"><span ></span></span>
 										</div>
 									</div>
 									<div class="form-group">
 										<label class="radio-inline">
-										  <input type="radio" name="Sex">Male
+										  <input type="radio" name="sex">Male
 										</label>
 										<label class="radio-inline">
-										  <input type="radio" name="Sex">Female
+										  <input type="radio" name="sex">Female
 										</label>
 									</div>
 									<div class="form-group">
-										<label for="InputUserName">Enter UserName</label>
+										<label for="usename">Enter UserName</label>
 										<div class="input-group">
-											<input type="text" class="form-control" name="InputUserName" id="InputUserName" placeholder="Enter UserName" required>
+											<input type="text" class="form-control" name="usename" id="usename" placeholder="Enter UserName" required>
 											<span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="InputPassword">Enter Password</label>
+										<label for="password">Enter Password</label>
 										<div class="input-group">
-											<input type="password" class="form-control" id="InputPassword" name="InputPassword" placeholder="Enter Password" required>
+											<input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" required>
 											<span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="InputEmail">Enter Email</label>
+										<label for="mail">Enter Email</label>
 										<div class="input-group">
-											<input type="email" class="form-control" id="InputEmailFirst" name="InputEmail" placeholder="Enter Email" required>
+											<input type="email" class="form-control" id="mail" name="mail" placeholder="Enter Email" required>
 											<span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="InputPhone">Enter Phone</label>
+										<label for="phone">Enter Phone</label>
 										<div class="input-group">
-											<input type="number" class="form-control" id="InputPhone" name="InputPhone" placeholder="Enter Phone" >
+											<input type="number" class="form-control" id="phone" name="phone" placeholder="Enter Phone" >
 											<span class="input-group-addon"><span "></span></span>
 										</div>
 									</div>
 									<div class="form-group">
-										<label for="InputBirthday">Enter Birthday</label>
+										<label for="birthday">Enter Birthday</label>
 										<div class="input-group">
-											<input type="date" class="form-control" id="InputBirthday" name="InputBirthday"  >
+											<input type="date" class="form-control" id="birthday" name="birthday"  >
 											<span class="input-group-addon"><span ></span></span>
 										</div>
 									</div>
